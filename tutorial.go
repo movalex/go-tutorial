80cbddb5e41188a251e40f150d3bcf78eba76e4b
Alex Bogomolov <mail@abogomolov.com>
2021-12-03 22:03:54 +0300

    user input

--- tutorial.go
+++ tutorial.go
@@ -4,12 +4,7 @@ import "fmt"
 
 func main() {
 	fmt.Println("Welcome to my quiz!")
-
-	// var name string = "Alex"
-
-	name := "Alex" // guess type
-	age := 39
-
-	fmt.Printf("Hello %v, how are you doing? You are %v years old, sadly", name, age)
-
+	var name string
+	fmt.Scan(&name) // user input
+	fmt.Printf("Your name is %v!", name)
 }

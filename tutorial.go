12ff1854959ba92d0178d71efb4e224b849413c2
Alex Bogomolov <mail@abogomolov.com>
2021-12-03 22:10:13 +0300

    print bools

--- tutorial.go
+++ tutorial.go
@@ -4,7 +4,14 @@ import "fmt"
 
 func main() {
 	fmt.Println("Welcome to my quiz!")
+	fmt.Printf("Enter your name:\n")
 	var name string
 	fmt.Scan(&name) // user input
-	fmt.Printf("Your name is %v!", name)
+	fmt.Printf("Hello, %v! Welcome to the game!\n", name)
+
+	fmt.Printf("Enter your age: ")
+	var age int
+	fmt.Scan(&age)
+
+	fmt.Println(age >= 10) // returns bool
 }
